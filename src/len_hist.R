cdf736326dd7df86f21406cec8d683593e46cc86
vanya-antonov <vanya.antonov@gmail.com>
2021-05-27 11:32:30 +0300

    deepZ histogram

--- src/len_hist.R
+++ src/len_hist.R
@@ -3,7 +3,8 @@ source('lib.R')
 
 ###
 
-NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19'
+NAME <- 'DeepZ'
+#NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19'
 #NAME <- 'H3K4me3_A549.ENCFF573MUH.hg38'
 #NAME <- 'H3K4me3_A549.ENCFF832EOL.hg19'
 #NAME <- 'H3K4me3_A549.ENCFF832EOL.hg38'
@@ -11,7 +12,8 @@ NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19'
 ###
 
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
 
 ggplot(bed_df) +
